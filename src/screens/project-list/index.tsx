77c2bf8f912541b2d392e73517ebf931eb120695
magicmooc <riyingxiaozi@gmail.com>
2022-06-26 17:04:05 +0800

    style: 优化kanban样式

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,12 +1,16 @@
 import { useDebounce, useDocumentTitle } from "utils";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
-import styled from "@emotion/styled";
 import { useProjects } from "../../utils/project";
 import { useUsers } from "../../utils/user";
 import { useProjectsSearchParams } from "./util";
-import { ButtonNoPadding, ErrorBox, Row } from "../../components/lib";
 import { useProjectModal } from "screens/project-list/util";
+import {
+  ButtonNoPadding,
+  ErrorBox,
+  Row,
+  ScreenContainer,
+} from "components/lib";
 // 基本类型，可以放到依赖里；组件状态，可以放到依赖里；非组件状态的对象，绝不可以放到依赖里
 export const ProjectListScreen = () => {
   useDocumentTitle("项目列表", false);
@@ -20,7 +24,7 @@ export const ProjectListScreen = () => {
   const { data: users } = useUsers();
 
   return (
-    <Container>
+    <ScreenContainer>
       {/* <Helmet>
         <title>项目列表</title>
       </Helmet> */}
@@ -34,12 +38,8 @@ export const ProjectListScreen = () => {
       <SearchPanel users={users || []} param={param} setParam={setParam} />
       <ErrorBox error={error} />
       <List loading={isLoading} users={users || []} dataSource={list || []} />
-    </Container>
+    </ScreenContainer>
   );
 };
 
 ProjectListScreen.whyDidYouRender = false;
-
-const Container = styled.div`
-  padding: 3.2rem;
-`;
